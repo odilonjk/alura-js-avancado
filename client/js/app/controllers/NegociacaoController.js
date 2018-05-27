759e722a2c956d5baec79864fa13f34f05dbb357
Odilon Kroger <kroger.odilonj@gmail.com>
2018-05-27 08:35:17 -0300

    Added function to clean list

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,7 +5,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem = new Mensagem();
@@ -34,6 +34,13 @@ class NegociacaoController {
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+    }
+
+    limpaLista() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = "Lista de negociações apagada com sucesso!";
+        this._mensagemView.update(this._mensagem);
     }
 
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,22 @@
 class ListaNegociacoes {
 
-    constructor() {
-        this._negociacoes =[]
+    constructor(trap) {
+        this._negociacoes = [];
+        this._trap = trap;
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
+        this._trap(this);
     }
     
     get negociacoes() {
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        this._trap(this);
     }
 
 }
